8b4d138082e6e3ff133394444885ab89c7b2cf36
unknown <automatizacionig@gmail.com>
2022-11-02 11:05:59 -0400

    añadiendo bg y animacion

--- src/components/landing/LandingProducts.jsx
+++ src/components/landing/LandingProducts.jsx
@@ -1,10 +1,55 @@
 import React from 'react'
+import {
+  HStack,
+  Box,
+  Image,
+  Heading,
+  Center,
+  Grid,
+  GridItem
+} from '@chakra-ui/react'
 
 const LandingProducts = () => {
   return (
-    <div>
-      <h3>Productos</h3>
-    </div>
+    <Grid 
+    // color='brand.blue' 
+    py={6}
+    bgGradient='linear(to-b, brand.blue, brand.aquamarinePrimary)'
+    color='white'
+    >
+      <Heading>
+        <Center textAlign='center'>
+        Productos
+        </Center>
+      </Heading>
+      <Grid
+      gridTemplateColumns={['1fr', '1fr','1fr 1fr','1fr 1fr']}
+      gap={4}
+      textAlign='center'
+      padding={6}
+      >
+        <GridItem
+        border='1px solid blue'
+        borderRadius={50}
+        >Productos de limpieza</GridItem>
+        <GridItem
+        border='1px solid blue'
+        borderRadius={50}
+        >Bolsas plásticas</GridItem>
+        <GridItem
+        border='1px solid blue'
+        borderRadius={50}
+        >Papelería institucional</GridItem>
+        <GridItem
+        border='1px solid blue'
+        borderRadius={50}
+        >Papelería en general</GridItem>
+        <GridItem
+        border='1px solid blue'
+        borderRadius={50}
+        >Víveres en general</GridItem>
+      </Grid>
+    </Grid>
   )
 }
 
